3e320d8debbff868a388a508b7cd4997dce5ff79
letsouzars <leticia.souza17senai@gmail.com>
2023-08-07 16:48:42 -0300

    index
     v

--- controle de acesso em JS/main.js
+++ controle de acesso em JS/main.js
@@ -3,16 +3,23 @@ function verificarAcesso() {
     const mensagemElement = document.getElementById('mensagem');
   
     if (numeroCartao >= 1000 && numeroCartao <= 1999) {
-      mensagemElement.innerText = 'Acesso permitido a todas as áreas restritas.';
+      mensagemElement.innerText = 'Acesso permitido.';
+      mensagemElement.style.color="green";
+      
    
     } else if (numeroCartao >= 2000 && numeroCartao <= 2999) {
-      mensagemElement.innerText = 'Acesso limitado a algumas áreas restritas.';
+      mensagemElement.innerText = 'Acesso limitado.';
+      mensagemElement.style.color="yellow";
    
     } else if (numeroCartao > 3000) {
-      mensagemElement.innerText = 'Acesso negado. Funcionário não tem permissão para acessar áreas restritas.';
+      mensagemElement.innerText = 'Acesso negado.';
+      mensagemElement.style.color="red";
       
     } else {
       mensagemElement.innerText = 'Número de cartão não identificado';
+      mensagemElement.style.color="gray";
       
     }
   }
+
+ 
